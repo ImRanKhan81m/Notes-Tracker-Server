8ebc3be8189921a991feac152171dfbadecdd575
Imran Hossen <freelancerimran81@gmail.com>
2022-04-27 17:49:49 +0600

    done

--- index.js
+++ index.js
@@ -9,6 +9,15 @@ require('dotenv').config();
 
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xnxtp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
 
 
 
